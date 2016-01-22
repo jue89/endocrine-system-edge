3a893e00cfc3af670d2fe9fde5ab56cc0238d027
Jue <me@jue.yt>
2016-01-22 12:41:23 +0100

    Created some test cases

---
+++ test/es.js
@@ -0,0 +1,52 @@
+"use strict";
+
+let assert = require( 'assert' );
+
+
+describe( "Class EndocrineSystem", function() => {
+
+	before( ( done ) => {
+
+	} );
+
+	after( ( done ) => {
+
+	} );
+
+	it( "should reject connecting to the broker due to non-matching key and certificate", ( done ) => {
+
+	} );
+
+	it( "should reject connecting to the broker due to non-matching certificate and CA", ( done ) => {
+
+	} );
+
+	it( "should reject connecting to the broker due to too large time drift", ( done ) => {
+
+	} );
+
+	it( "should connect to the broker even with a large time drift, if the es shall ignore it", ( done ) => {
+
+	} );
+
+	it( "should fail to connect to the broker if it is not existent", ( done ) => {
+
+	} );
+
+	it( "should connect to the broker, emit the online event, get the online state, create a gland / receptor and then disconnect", ( done ) => {
+
+	} );
+
+	it( "should connect to the broker and emit the offline event, if the connection is lost and then reconnect to the broker", ( done ) => {
+
+	} );
+
+	it( "should connect to the broker and emit the offline event, if the connection is lost and then reconnect to the broker", ( done ) => {
+
+	} );
+
+	it( "should subscribe to a topic and publish / receive a message", ( done ) => {
+
+	} );
+
+} );
