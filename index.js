9345bc9fdf5ebda802418270a88f871b63543f67
Jue <me@jue.yt>
2016-02-29 20:55:51 +0100

    Exposing classes
    This may be handy for other modules to check instanceof against these classes.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 "use strict";
 
 const EndocrineSystem = require( './lib/es.js' );
+const Source = require( './lib/source.js' );
+const Sink = require( './lib/sink.js' );
+const Definition = require( './lib/definition.js' );
+const Hormone = require( './lib/hormone.js' );
 
 module.exports = function( options ) {
 	return new EndocrineSystem( options );
+};
+
+module.exports.Classes = {
+	EndocrineSystem,
+	Source,
+	Sink,
+	Definition,
+	Hormone
 };
