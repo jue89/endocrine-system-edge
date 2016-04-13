49ac066ba3c3eb883f0444ae1954a9e9753d99dd
Jue <me@jue.yt>
2016-04-13 23:04:38 +0200

    Changed Sink class to new subscribe / unsubscribe API

--- test/mocks/es.js
+++ test/mocks/es.js
@@ -8,6 +8,7 @@ class ESMock {
 		this._ca = ca;
 		this._onsub = {};
 		this._definitionResendInterval = 60;
+		this._handles = [];
 	}
 
 	_publish( topic, payload ) {
@@ -17,14 +18,19 @@ class ESMock {
 	}
 
 	_subscribe( topic, handler ) {
+		let handle = this._handles.push( true ) - 1;
 		if( this._onsub[ topic ] && typeof this._onsub[ topic ] == 'function' ) {
-			return this._onsub[ topic ]( handler );
+			return this._onsub[ topic ]( handler ).then( () => {
+				return handle;
+			} );
+		} else {
+			return Promise.resolve( handle );
 		}
-		return Promise.resolve();
 	}
 
-	_unsubscribe( topic ) {
-		return Promise.resolve();
+	_unsubscribe( handle ) {
+		if( this._handles[ handle ] !== undefined ) return Promise.resolve();
+		else return Promise.reject( new Error( "Unknown subscription handle" ) );
 	}
 
 }
