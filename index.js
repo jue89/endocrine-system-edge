fdeab7aaedd16c8ffd9448ff57069b3f024e9287
Jue <me@jue.yt>
2016-12-30 03:34:29 +0100

    Added table of all occurring events

--- index.js
+++ index.js
@@ -17,3 +17,28 @@ module.exports.Classes = {
 	Definition,
 	Hormone
 };
+
+// Table of all events that might be emitted by ES instances.
+// Format: each event has its own translation function, that outputs a (mostly)
+// human-readable message (I'm not a Goethe, sry!) and an object representing
+// the environment in that the event occured.
+module.exports.eventTable = {
+	'connecting': ( env ) => [ 'debug', `Connecting to ${env.url} ...`, env ],
+	'error': ( err, env ) => [ 'err', err.message, env ],
+	'online': ( env ) => [ 'notice', `Connected to ${env.url}`, env ],
+	'offline': ( env ) => [ 'notice', `Disconnected from ${env.url}`, env ],
+	'newReceptor': ( env ) => [ 'debug', `New receptor listening for ${env.filter}`, env ],
+	'removedReceptor': ( env ) => [ 'debug', `Removed receptor listening for ${env.filter}`, env ],
+	'receptionError': ( err, env ) => [ 'notice', err.message, env ],
+	'defined': ( env ) => [ 'debug', `Received definition and subscribed for ${env.name} hormones`, env ],
+	'refreshed': ( env ) => [ 'debug', `Refreshed definition for ${env.name} hormones`, env ],
+	'undefined': ( env ) => [ 'debug', `Unsubscribed form ${env.name} hormones`, env ],
+	'hormoneRefresh': ( env ) => [ 'debug', `Hormone ${env.name} has been refreshed`, env ],
+	'hormoneExpiration': ( env ) => [ 'debug', `Hormone ${env.name} has expired`, env ],
+	'hormoneRecovery': ( env ) => [ 'debug', `Hormone ${env.name} has been recovered`, env ],
+	'hormoneError': ( env ) => [ 'debug', `Hormone ${env.name} reported an error: ${env.error}`, env ],
+	'hormone': ( env ) => [ 'debug', `Received ${env.name} hormone`, env ],
+	'newGland': ( env ) => [ 'debug', `Defined gland emitting ${env.name} hormones`, env ],
+	'removedGland': ( env ) => [ 'debug', `Removed gland emitting ${env.name} hormones`, env ],
+	'sentHormone': ( env ) => [ 'debug', `Sent ${env.name} hormone`, env ]
+};
